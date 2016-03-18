c0c067ecbe8b0de911e1dfa061311ed5ad9713f4
danieltunon <danieltunon@gmail.com>
2016-03-18 16:14:02 -0700

    Implement dynamic video player component

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer exampleVideo={exampleVideoData[0]} />
     </div>
     <div className="col-md-5">
       <VideoList videos={exampleVideoData} />
