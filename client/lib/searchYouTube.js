2717931e676f453831e42001891020c878415037
danieltunon <danieltunon@gmail.com>
2016-03-18 20:07:34 -0700

    Begin ajax youtube search api

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    password: YOUTUBE_API_KEY,
+    dataType: 'json',
+    contentType: 'application/json',
+    data: options.query,
+    success: function(data) {console.log(data)},
+    error: function(e) {console.dir(e)}
+  });
 };
 
 window.searchYouTube = searchYouTube;
