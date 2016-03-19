e70d83a1086afcc6289dd056a831f0d140f6c130
danieltunon <danieltunon@gmail.com>
2016-03-18 19:25:58 -0700

    Hook together video list and video player componenets:wq!

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,63 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer exampleVideo={exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData} />
-    </div>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById('app'));
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer exampleVideo={exampleVideoData[0]} />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={exampleVideoData} />
+//     </div>
+//   </div>
+// );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo: props.videos[0],
+      videoList: props.videos
+    };
+
+  }
+
+  clickHandler(clickedMovie) {
+    this.setState({
+      currentVideo: clickedMovie
+    });
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList} handler={this.clickHandler.bind(this)}/>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer exampleVideo={exampleVideoData[0]} />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={exampleVideoData} />
+//     </div>
+//   </div>
+// );
+
+
+
+
+ReactDOM.render(<App videos={exampleVideoData}/>, document.getElementById('app'));
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -9,11 +9,11 @@ class VideoPlayer extends React.Component {
     return (
       <div className="video-player">
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${this.props.exampleVideo.id.videoId}`} allowFullScreen></iframe>
+          <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${this.props.video.id.videoId}`} allowFullScreen></iframe>
         </div>
         <div className="video-player-details">
-          <h3>{this.props.exampleVideo.snippet.title}</h3>
-          <div>{this.props.exampleVideo.snippet.description}</div>
+          <h3>{this.props.video.snippet.title}</h3>
+          <div>{this.props.video.snippet.description}</div>
         </div>
       </div>
     );
